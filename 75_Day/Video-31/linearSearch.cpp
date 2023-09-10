89b345ec00ba9a8319d1c3c7678a02e825af8e6a
Harsh <harshdivate45@gmail.com>
2023-09-10 22:14:10 +0530

    Linked List

--- 75_Day/Video-31/linearSearch.cpp
+++ 75_Day/Video-31/linearSearch.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 #include<bits/stdc++.h>
 
+
+bool LinearSearch(int *arr,int size,int ele){
+    if(size==0){
+        return false;
+    }
+    if(arr[0]==ele){
+        return true;
+    }else{
+        bool remainingPart=LinearSearch(arr+1,size-1,ele);
+        return remainingPart;
+    }
+
+}
 int main() {
+    int arr[5]={2,4,5,1,44};
+    int ele=2;
+    bool ans=LinearSearch(arr,4,ele);
+    if (ans) {
+        cout<<"Found"<<endl;
+    }
     
     return 0;
 }
