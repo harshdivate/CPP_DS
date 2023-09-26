36115e34572b7c30d4cfb23a434786b86fd8675e
Harsh <harshdivate45@gmail.com>
2023-09-26 23:12:13 +0530

    Video-44 Done

--- LinkedList/CircularLL.cpp
+++ LinkedList/CircularLL.cpp
@@ -37,17 +37,65 @@ void insertAtHead(Node* &head,int d){
     head=newInsert;
 }
 
+void insertAtTail(Node* &head,int data){
+    if (head==NULL){
+        insertAtHead(head,data);
+    }
+    Node* newNode=new Node(data);
+    Node* temp=head;
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+    
+
+}
+
+void deletion(Node* &head,int position){
+    if (position==1){
+        Node* temp=head;
+        while(temp->next!=head){
+            temp=temp->next;
+        }
+        Node* todelete=head;
+        temp->next=head->next;
+        head=head->next;
+        todelete->next=NULL;
+        delete todelete;
+    }
+    else{
+        Node* temp=head;
+        int cnt=1;
+        while(cnt<position){
+            temp=temp->next;
+            cnt++;
+        }
+        Node* todelete=temp->next;
+        
+        temp->next=temp->next->next;
+        todelete->next=NULL;
+        delete todelete;
+
+    }
+}
+
 void print(Node* &head){
     Node* temp=head;
    do{
     cout<<temp->data<<" ";
     temp=temp->next;
-   }while(temp!=NULL);
+   }while(temp!=head);
 }
 
 int main(){
     Node* head=NULL;
     insertAtHead(head,100);
+    insertAtHead(head,200);
+    insertAtHead(head,300);
+    insertAtTail(head,99);
+    deletion(head,1);
+    
     print(head);
 
     return 0;
