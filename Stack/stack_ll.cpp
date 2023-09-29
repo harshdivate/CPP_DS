cd2e4d412501ede6a6489acc4a2ee246ad24e5e4
Harsh <harshdivate45@gmail.com>
2023-09-29 23:35:50 +0530

    Stack Implemtation with LL

--- Stack/stack_ll.cpp
+++ Stack/stack_ll.cpp
@@ -1,35 +1,86 @@
 #include<iostream>
 using namespace std;
 
+
 class Node{
     public:
     int data;
     Node* next;
+    Node* top=NULL;
 
     Node(int data){
         this->data=data;
         this->next=NULL;
     }
-}
 
-
-void push(Node* &top,int data){
-    if(top==NULL){
-        Node* newInsert=new Node(data);
-        top=newInsert;
+    ~Node(){
+        int value=this->data;
+        if(this->next!=NULL){
+            delete next;
+            next=NULL;
+        }
+        cout<<"Data deleted"<<value<<endl;
     }
-    else{
-            if(top!=NULL){
+
+    void push(int data){
+        if(top==NULL){
+            Node* newHead=new Node(data);
+            top=newHead;
+        }else{
             Node* newInsert=new Node(data);
             newInsert->next=top;
             top=newInsert;
-            }else{
-                cout<<"Stack Overflow"<<endl;
-            }
-            
         }
+    }
 
+    void pop(){
+        if(top==NULL){
+            cout<<"Stack Underflow"<<endl;
+            return;
+        }else{
+            Node* temp=top;
+            top=top->next;
+            temp->next=NULL;
+            delete temp;
+        }
+    }
+    int peek(){
+        if(top==NULL){
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+            return top->data;
+    }
 
-    void pop()
+    bool isEmpty(){
+        if(top==NULL){
+            return true;
+        }
+        return false;
+    }
+    void print(){
+        Node* temp=top;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+};
 
+int main(){
+    Node* s;
+    s->push(100);
+    s->push(200);
+    s->push(300);
+    
+    s->print();
+    cout<<"PEeak"<<s->peek()<<endl;
+    s->pop();
+    s->pop();
+    s->pop();
+    s->pop();
+    s->print();
+    cout<<"Stack is Empty?"<<s->isEmpty()<<endl;
+    return 0;
 }
+
